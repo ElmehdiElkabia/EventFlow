c6b65d414f8fa4692e055e5b033bc79a07c5f442
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-03-07 15:20:23 +0000

    feat: Add EventApprovedNotification class for notifying users about event approval via email

--- backend/app/Notifications/EventApprovedNotification.php
+++ backend/app/Notifications/EventApprovedNotification.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class EventApprovedNotification extends Notification
 {
@@ -18,7 +19,20 @@ class EventApprovedNotification extends Notification
 
     public function via($notifiable)
     {
-        return ['database'];
+        return ['database', 'mail'];
+    }
+
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Event Approved - ' . $this->event->title)
+            ->greeting('Hello ' . $notifiable->name . '!')
+            ->line('Great news! Your event has been approved.')
+            ->line('**Event:** ' . $this->event->title)
+            ->line('**Status:** Approved')
+            ->line('This event will be published and visible to all users.')
+            ->action('View Event', url('/events/' . $this->event->id))
+            ->line('Thank you for using our platform!');
     }
 
     public function toArray($notifiable)
