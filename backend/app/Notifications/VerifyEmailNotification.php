4e794d18cc0cf33a0f63fd7848cb6e952d7d1622
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-03-07 15:20:35 +0000

    feat: Add VerifyEmailNotification class for notifying users to verify their email address via email

---
+++ backend/app/Notifications/VerifyEmailNotification.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\URL;
+
+class VerifyEmailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $verificationUrl = $this->verificationUrl($notifiable);
+
+        return (new MailMessage)
+            ->subject('Verify Email Address')
+            ->line('Please click the button below to verify your email address.')
+            ->action('Verify Email Address', $verificationUrl)
+            ->line('If you did not create an account, no further action is required.');
+    }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     */
+    protected function verificationUrl($notifiable)
+    {
+        $frontendUrl = config('app.frontend_url', 'http://localhost:5173');
+        
+        $hash = sha1($notifiable->getEmailForVerification());
+        
+        return $frontendUrl . '/verify-email/' . $notifiable->getKey() . '/' . $hash;
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
