d0f53bea42f15b08c6ba86d69fec10f0c54d7324
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-03-10 01:25:58 +0000

    refactor: Update CORS configuration to use FRONTEND_URL for allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -3,8 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS')],
-
+    'allowed_origins' => [env('FRONTEND_URL')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
