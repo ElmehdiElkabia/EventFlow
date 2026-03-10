ad88fd5f49565c9ad6e217298151b5fbd508573e
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-03-10 18:27:55 +0000

    refactor: Add SpeedInsights component to main rendering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <SpeedInsights />
     </AuthProvider>
   </StrictMode>,
 )
